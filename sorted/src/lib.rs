a3af6f614f6320a6ef6266604141a79ad7a5960a
David <dmcfarland8031@gmail.com>
2024-08-11 20:18:03 -0600

    All 8 tests completed! Yay me

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,5 +1,5 @@
-#![allow(unused_imports, unused_variables)]
-#![allow(dead_code)]
+//#![allow(unused_imports, unused_variables)]
+//#![allow(dead_code)]
 
 extern crate proc_macro;
 use proc_macro::TokenStream;
@@ -7,9 +7,8 @@ use proc_macro2::Span;
 use proc_macro2::TokenStream as TokenStream2;
 use quote::{quote, ToTokens};
 use std::fmt;
-use syn::fold::fold_expr_match;
 use syn::{
-    parse_macro_input, parse_quote,
+    parse_macro_input,
     visit_mut::{self, VisitMut},
 };
 
@@ -17,8 +16,8 @@ use syn::{
 pub fn sorted(_attrs: TokenStream, item: TokenStream) -> TokenStream {
     let mut out = item.clone();
     let mut stream: syn::Item = parse_macro_input!(item as syn::Item);
-    let data = LexicographicMatch::default();
-    match item_strategy::<TokenStream2>(data, &mut stream) {
+    let data = LexicographicMatch;
+    match item_strategy(data, &mut stream) {
         Ok(val) => val.into_token_stream().into(),
         Err(err) => {
             out.extend(TokenStream::from(err.to_compile_error()));
@@ -29,104 +28,49 @@ pub fn sorted(_attrs: TokenStream, item: TokenStream) -> TokenStream {
 
 #[proc_macro_attribute]
 pub fn check(_attrs: TokenStream, item: TokenStream) -> TokenStream {
-    let mut out = item.clone();
     let mut stream: syn::Item = parse_macro_input!(item as syn::Item);
-    let data = LexicographicMatch::default();
-    match item_strategy::<TokenStream2>(data, &mut stream) {
-        Ok(val) => return val.into_token_stream().into(),
-        Err(err) => {
-            out.extend(TokenStream::from(err.to_compile_error()));
-            return out;
-        }
-    };
+    let data = LexicographicMatch;
+    match item_strategy(data, &mut stream) {
+        Ok(val) => val.into_token_stream().into(),
+        Err(err) => TokenStream::from(err.to_compile_error()),
+    }
 }
 
 #[derive(Debug, Default)]
 struct LexicographicMatch;
 
-fn item_strategy<R>(
+fn item_strategy(
     mut data: impl VisitMut + fmt::Debug,
     stream: &mut syn::Item,
 ) -> syn::Result<TokenStream2> {
     match stream {
         syn::Item::Enum(val) => {
-            let pathes: Vec<syn::Path> = collect_pathes(val.variants.clone())?;
+            let pathes: Vec<syn::Path> = collect_pathes(&val.variants)?;
             match lexicographic_sort(&pathes, pathes.len()) {
-                Ok(()) => return Ok(stream.into_token_stream()),
-                Err(err) => return Err(err),
-            };
+                Ok(()) => Ok(stream.into_token_stream()),
+                Err(err) => Err(err),
+            }
         }
         syn::Item::Fn(val) => {
-            let expr_match = &val
+            data.visit_item_fn_mut(val);
+            let expr_match = val
                 .block
                 .stmts
                 .iter()
-                .find_map(|stmt| is_match_expr(&stmt))
+                .find_map(is_match_expr)
                 .expect("Failed to find match expression in item strat.");
-            let pathes = collect_pathes(expr_match.arms.clone())?;
-            data.visit_item_fn_mut(val);
-            let sort_error = lexicographic_sort(&pathes, pathes.len()).err();
-            if let Some(error) = sort_error {
+            let pathes = collect_pathes(expr_match.arms)?;
+            if let Some(error) = lexicographic_sort(&pathes, pathes.len()).err() {
                 let comp_error = error.to_compile_error();
-                return Ok(quote! { #val #comp_error});
+                Ok(quote! { #val #comp_error })
             } else {
-                return Ok(quote! { #val});
+                Ok(quote! { #val })
             }
         }
-        _ => {
-            return Err(syn::Error::new(
-                Span::call_site(),
-                "expected enum or match expression",
-            ))
-        }
-    }
-}
-
-fn is_match_expr(expr: &syn::Stmt) -> Option<syn::ExprMatch> {
-    if let syn::Stmt::Expr(stmt_expr, _) = expr {
-        if let syn::Expr::Match(expr_match) = stmt_expr {
-            return Some(expr_match.clone());
-        } else {
-            return None;
-        }
-    } else {
-        return None;
-    }
-}
-
-fn is_sorted_flag(attr: &syn::Attribute) -> Option<()> {
-    if attr.meta.path().is_ident("sorted") {
-        return Some(());
-    } else {
-        return None;
-    }
-}
-
-fn item_fn_strategy(
-    mut data: impl VisitMut + fmt::Debug,
-    i: &mut syn::ItemFn,
-) -> syn::Result<TokenStream2> {
-    data.visit_item_fn_mut(i);
-    let expr_match = &i
-        .block
-        .stmts
-        .iter()
-        .find_map(|stmt| is_match_expr(&stmt))
-        .expect("Failed to find stmt in fn strat.");
-    let pathes = collect_pathes(expr_match.arms.clone())?;
-    match lexicographic_sort(&pathes, pathes.len()) {
-        Ok(()) => {
-            if let Some(_) = expr_match
-                .attrs
-                .iter()
-                .find_map(|attr| is_sorted_flag(&attr))
-            {
-                return Ok(quote! { #i });
-            } else {
-                panic!()
-            }
-        }
-        Err(err) => return Err(err),
+        _ => Err(syn::Error::new(
+            Span::call_site(),
+            "expected enum or match expression",
+        )),
     }
 }
 
@@ -134,10 +78,7 @@ fn lexicographic_sort(pathes: &Vec<syn::Path>, len: usize) -> syn::Result<()> {
     let mut sorted_list: Vec<String> = Vec::with_capacity(len);
     for path in pathes {
         if !path.segments.is_empty() {
-            let name = path
-                .get_ident()
-                .expect("path.get_ident() failed.")
-                .to_string();
+            let name = full_path_name(path);
             if sorted_list
                 .last()
                 .map(|last_item| &name < last_item)
@@ -170,26 +111,25 @@ impl visit_mut::VisitMut for LexicographicMatch {
     }
 }
 
-trait FullPath {
-    fn full_path(&self) -> String;
-}
-
-impl FullPath for syn::Path {
-    fn full_path(&self) -> String {
-        return self.segments.iter().fold(String::new(), |name, iter| {
-            format!("{}::{}", name, iter.ident)
-        });
-    }
-}
-
 trait IntoPath {
     fn into_path(self) -> syn::Result<syn::Path>;
 }
 
-impl IntoPath for syn::Variant {
+impl IntoPath for syn::Ident {
     fn into_path(self) -> syn::Result<syn::Path> {
         let mut segments = syn::punctuated::Punctuated::new();
-        segments.push_value(self.ident.into());
+        segments.push_value(self.into());
+        Ok(syn::Path {
+            leading_colon: None,
+            segments,
+        })
+    }
+}
+
+impl IntoPath for &syn::Variant {
+    fn into_path(self) -> syn::Result<syn::Path> {
+        let mut segments = syn::punctuated::Punctuated::new();
+        segments.push_value(self.ident.clone().into());
         Ok(syn::Path {
             leading_colon: None,
             segments,
@@ -203,6 +143,21 @@ impl IntoPath for syn::Arm {
             syn::Pat::Path(pat_path) => Ok(pat_path.path),
             syn::Pat::TupleStruct(tpl_pat) => Ok(tpl_pat.path),
             syn::Pat::Struct(pat_struct) => Ok(pat_struct.path),
+            syn::Pat::Wild(syn::PatWild { attrs, .. }) => {
+                if attrs.is_empty() {
+                    Ok(syn::Path {
+                        leading_colon: None,
+                        segments: syn::punctuated::Punctuated::new(),
+                    })
+                } else {
+                    Ok(attrs[0].meta.path().clone())
+                }
+            }
+            syn::Pat::Ident(syn::PatIdent {
+                ident,
+                subpat: None,
+                ..
+            }) => Ok(ident.into_path().expect("Couldn't convert into_path().")),
             other => Err(syn::Error::new_spanned(other, "unsupported by #[sorted]")),
         }
     }
@@ -214,4 +169,23 @@ where
     I::Item: IntoPath,
 {
     iter.into_iter().map(IntoPath::into_path).collect()
+}
+
+fn is_match_expr(expr: &syn::Stmt) -> Option<syn::ExprMatch> {
+    if let syn::Stmt::Expr(syn::Expr::Match(expr_match), _) = expr {
+        Some(expr_match.clone())
+    } else {
+        None
+    }
+}
+
+fn full_path_name(path: &syn::Path) -> String {
+    if path.segments.len() == 1 {
+        return path.segments[0].ident.to_string();
+    }
+    path.segments
+        .iter()
+        .map(|seg| format!("{}", quote! { #seg }))
+        .collect::<Vec<_>>()
+        .join("::")
 }
